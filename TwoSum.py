5ab4b9d6e1d0904f1f2fe1dd8aa6cd59860c3d0c
M0M01992 <tseeweichen@buaa.edu.cn>
2017-07-19 20:40:02 +0800

    Update TwoSum.py

--- TwoSum.py
+++ TwoSum.py
@@ -1,32 +1,30 @@
+import urllib
 import bisect
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        numsb = sorted(nums);
-        flag = 0;
-        i = 0;
-        endi = len(numsb);
-        for i in range(0,len(numsb)):
-            if(numsb[i] > target):
-                endi = i;
+def twoSum( nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    numsb = sorted(nums);
+    flag = 0;
+    i = 0;
+    for i in range(0,len(numsb)):
+        checki = bisect.bisect(numsb,target - numsb[i]);
+        if(numsb[checki-1] == target - numsb[i] ):
+            flag = 1;
+            a = numsb[i];
+            break;
+    if(flag == 1):
+        for i in range(0,len(nums)):
+            if(nums[i]==a or nums[i] == target - a):
+                aa = i;
+                a = nums[i];
+                for j in range(i+1, len(nums)):
+                    if(nums[j]==target - a):
+                        bb = j;
                 break;
-        for i in range(0,endi):
-            checki = bisect.bisect(numsb,target - numsb[i]);
-            if(numsb[checki-1] == target - numsb[i] ):
-                #if numsb[i]*2 != target:
-                flag = 1;
-                a = numsb[i];
-                break;
-        if(flag == 1):
-            for i in range(0,len(nums)):
-                if(nums[i]==a):
-                    aa = i;
-                if(nums[i]==target - a):
-                    bb = i;
-            return [aa,bb];
-        else:
-            return [0,0];
+
+        return sorted([aa,bb]);
+    else:
+        return [0,0];
